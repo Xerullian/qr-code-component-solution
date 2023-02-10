6998cb106f66ea085723fbc50ae76660d0d9bc16
Patrick Nolen <xerullian@gmail.com>
2023-02-10 15:01:00 -0500

    build(tailwind): Update base styles

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,23 @@
-/** @type {import('tailwindcss').Config} */
+// /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        white: "hsl(0, 0%, 100%)",
+        "light-gray": "hsl(212, 45%, 89%)",
+        "grayish-blue": "hsl(220, 15%, 55%)",
+        "dark-blue": "hsl(218, 44%, 22%)",
+      },
+      fontFamily: {
+        outfit: ["Outfit"],
+      },
+      fontSize: {
+        base: "15px",
+      },
+    },
+  },
+  plugins: [],
+};
