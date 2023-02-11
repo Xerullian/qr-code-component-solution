6036aa91b918bd9fe814beacc9d130d5713fef1b
Patrick Nolen <xerullian@gmail.com>
2023-02-11 14:11:05 -0500

    ci(astro config): Add missing base parameter

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://xerullian.github.io',
+    base: '/qr-code-component-solution',
     integrations: [tailwind()],
 });
